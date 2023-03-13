e78d2e20af91771e08a7ae67d7a67f9cafc3cc5f
Havrylenko Illia <76633174+GIDKarazin@users.noreply.github.com>
2023-03-13 23:42:07 +0200

    Updating 'ValidParenthesesCheck.rb' file
    The 'ValidParenthesesCheck.rb' file was updated with the "Algorithm of valid parentheses check" code. Eventually, the "Hello, World" code was removed from file.

--- ValidParenthesesCheck.rb
+++ ValidParenthesesCheck.rb
@@ -1,1 +1,21 @@
-puts "Hello, World!"
+def valid_parentheses?(string)
+  stack = []
+  mapping = { '(' => ')', '{' => '}', '[' => ']' }
+
+  string.each_char do |char|
+    case char
+    when *mapping.keys
+      stack.push(char)
+    when *mapping.values
+      return false unless stack.pop == mapping.key(char)
+    end
+  end
+
+  stack.empty?
+end
+
+puts valid_parentheses?('(){}[]')   # => true
+puts valid_parentheses?('([{}])')   # => true
+puts valid_parentheses?('(}')       # => false
+puts valid_parentheses?('[(])')     # => false
+puts valid_parentheses?('[({})](]') # => false
